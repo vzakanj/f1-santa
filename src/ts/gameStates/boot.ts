9bb6364fb06d6d50838368ad5eb51618cd2756f2
luka erkapic <luka erkapic>
2016-09-10 22:53:05 +0200

    intro state

--- src/ts/gameStates/boot.ts
+++ src/ts/gameStates/boot.ts
@@ -3,20 +3,49 @@
 
 
 
-   export  class Boot extends Phaser.State {
+export class Boot extends Phaser.State {
 
-        preload(): void {
-            this.load.image('logo', './assets/cat.png');
-        }
+    preload(): void {
+        this.load.image('logo', './assets/mono-logo.png');
+        this.load.image('gamelogo', './assets/cat.png');
+    }
 
-        create(): void {
-            this.game.stage.backgroundColor = "#659CEF";
-            this.scale.pageAlignHorizontally = true;
+    create(): void {
 
-            var sprite = this.game.add.sprite(0, 0, 'logo');
-        }
+        this.game.stage.backgroundColor = "#659CEF";
+        this.scale.pageAlignHorizontally = true;
+
+        var monoLogo = this.game.add.sprite(this.world.centerX, this.world.centerY, 'logo');
+        monoLogo.anchor = new Phaser.Point(0.5, 0.5);
+        monoLogo.alpha = 0;
+
+        // Logo animation
+        var monoLogoAnimation = this.game.add.tween(monoLogo).to({ alpha: 1 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, true);
+        monoLogoAnimation.onComplete.add(function () { 
+            
+            // Game logo animation, falling
+            var gameLogo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'gamelogo');
+            gameLogo.anchor.set(0.5);
+            var gameLogoAnimation = this.game.add.tween(gameLogo).from({ y: -200 }, 2000, Phaser.Easing.Bounce.Out, true);
+            gameLogoAnimation.onComplete.add(function () {
+                
+                // Game logo fading animation
+                gameLogoAnimation.to({ alpha: 0 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, true);
+                gameLogoAnimation.onComplete.add(function () {
+                    this.state.start('Preload');
+                }, this);
+            }, this);
+
+        }, this);
+        
+        
+    }
+
+    update(): void {
 
     }
+
+}
 
 
 
